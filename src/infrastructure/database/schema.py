3f3449b5e97957145d5d6c6696eb8cc88967acfd
Vadim <netstrrex@gmail.com>
2025-09-05 00:04:08 +0300

    :bug: fix syntax error

--- src/infrastructure/database/schema.py
+++ src/infrastructure/database/schema.py
@@ -1,13 +1,13 @@
 SCHEMA = """
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXIST categories (
+CREATE TABLE IF NOT EXISTS categories (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     parent_id UUID REFERENCES categories(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXIST products (
+CREATE TABLE IF NOT EXISTS products (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     quantity INTEGER NOT NULL DEFAULT 0,
@@ -15,19 +15,19 @@ CREATE TABLE IF NOT EXIST products (
     category_id UUID REFERENCES categories(id) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXIST clients (
+CREATE TABLE IF NOT EXISTS clients (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     address TEXT
 );
 
-CREATE TABLE IF NOT EXIST orders (
+CREATE TABLE IF NOT EXISTS orders (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     client_id UUID NOT NULL REFERENCES clients(id) ON DELETE CASCADE,
     order_date TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXIST order_items (
+CREATE TABLE IF NOT EXISTS order_items (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     order_id UUID NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
     product_id UUID NOT NULL REFERENCES products(id) ON DELETE RESTRICT,
